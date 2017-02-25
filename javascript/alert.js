40f820e0b896de01dc7a9b8d699149bc953f68e7
Samuel Chittenden <sbchittenden@gmail.com>
2017-02-25 16:58:41 -0700

    Added alert.js for alert components, animation on dismissal

---
+++ javascript/alert.js
@@ -0,0 +1,44 @@
+(function() {
+  var alertComponents = document.querySelectorAll('[class^="c-alert--"], .c-alert');
+  console.log('alert components: ', alertComponents);
+  var numOfAlertComponents = alertComponents.length;
+  var i;
+
+  // add event listeners
+  for (i = 0; i < numOfAlertComponents; i++) {
+    alertComponents[i].addEventListener('click', _removeAlert(i));
+  }
+
+  function _removeAlert(i) {
+    return function(e) {
+      e.preventDefault();
+      // grab targeted alert component
+      var targetAlert = alertComponents[i];
+      // grab the close icon
+      var closeIcon = targetAlert.querySelector('.c-alert__close-alert');
+
+      // set to true if target is the close icon span or icon itself
+      var closeClicked = (e.target.nodeName === 'I' && e.target.parentNode.classList.contains('c-alert__close-alert')) || e.target.classList.contains('c-alert__close-alert') ? true : false;
+
+      if (closeClicked) {
+        // closing animation
+        targetAlert.classList.add('c-alert--remove');
+        // after animation runs remove alert from DOM
+        targetAlert.addEventListener('animationend', function() {
+          targetAlert.remove();
+        });
+      }
+
+    };
+  }
+
+
+
+
+
+
+
+
+
+})();
+
