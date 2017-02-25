0e8f650ae97993759b38f6fc1a2ee0e21a4e56b8
aashishnagpal <aashish.nagpal@hotmail.com>
2017-02-26 02:31:56 +0530

    Update Navs and Scrollspy - Implemented Review Comments

--- javascript/scrollspy-nav.js
+++ javascript/scrollspy-nav.js
@@ -5,31 +5,41 @@
   'use strict';
   var nav = document.querySelector('.nav');
   var links = nav.querySelectorAll('a[href]:not([href="#"]');
-  var targets = {};
+  var targets = [];
 
+  var clientHeight = window.innerHeight;
   Array.prototype.forEach.call(links, function (element) {
-    var id = element.hash.slice(1);
-    targets[id] = {
-      element: element.parentNode,
-      position: document.getElementById(id).offsetTop
-    }
+    var domTarget = document.getElementById(element.hash.slice(1));
+    targets.push({
+      navItem: element.parentNode,
+      domTarget: domTarget,
+      top: domTarget.offsetTop,
+      height: domTarget.offsetHeight + domTarget.offsetTop
+    });
   });
 
 
   var scrollspyNav = function () {
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-    for (var targetKey in targets) {
-      var previousTarget = nav.querySelector('.nav__item--active');
-      var currentTargetObj = targets[targetKey];
-      // var target = document.querySelector('a[href="#'+section+'"]');
-      if (currentTargetObj.position <= (scrollPosition + (2 * nav.offsetHeight))) {
-        if (previousTarget !== null)
-          previousTarget.classList.remove('nav__item--active');
-        currentTargetObj.element.classList.add('nav__item--active');
+    if (nav.classList.contains('nav--horizontal')) {
+      scrollPosition += nav.offsetHeight;
+    }
+
+    for (var i = 0; i < targets.length; i++) {
+      var checkTarget = targets[i];
+
+      if (scrollPosition >= checkTarget.top && scrollPosition <= checkTarget.top + checkTarget.height) {
+        checkTarget.navItem.classList.add('nav__item--active');
+        if (i > 0) {
+          targets[i - 1].navItem.classList.remove('nav__item--active');
+        }
+      } else {
+        checkTarget.navItem.classList.remove('nav__item--active');
       }
     }
   };
+
   window.addEventListener('load', function () {
     window.addEventListener('scroll', scrollspyNav);
   });
--- javascript/sticky-nav.js
+++ javascript/sticky-nav.js
@@ -4,18 +4,19 @@
 (function () {
   'use strict';
   var nav = document.querySelector('.nav[data-nav-type="sticky"]');
-
-  var initialOffset = nav.offsetTop;
-  var stickyNav = function () {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    if (initialOffset > scrollPosition) {
-      nav.classList.remove('nav--sticky');
-    }
-    else if (nav.offsetTop <= scrollPosition) {
-      nav.classList.add('nav--sticky');
-    }
-  };
-  window.addEventListener('load', function () {
-    window.addEventListener('scroll', stickyNav);
-  });
+  if (nav !== null) {
+    var initialOffset = nav.offsetTop;
+    var stickyNav = function () {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+      if (initialOffset > scrollPosition) {
+        nav.classList.remove('nav--sticky');
+      }
+      else if (nav.offsetTop <= scrollPosition) {
+        nav.classList.add('nav--sticky');
+      }
+    };
+    window.addEventListener('load', function () {
+      window.addEventListener('scroll', stickyNav);
+    });
+  }
 })();
