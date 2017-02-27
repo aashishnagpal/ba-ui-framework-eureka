057287087bf1858563aefafce9bd82b2479fa169
aashishnagpal <aashish.nagpal@hotmail.com>
2017-02-27 07:09:48 +0530

    Update sticky-nav

--- javascript/sticky-nav.js
+++ javascript/sticky-nav.js
@@ -7,10 +7,8 @@
   if (nav !== null) {
     var scrollContent = document.getElementById(nav.getAttribute('data-spyOn')) || document.documentElement || document.body;
 
-    var initialPosition = document.querySelector('header').getBoundingClientRect().bottom;
-    var finalPosition = document.querySelector('footer').getBoundingClientRect().top;
-
-    console.log(initialPosition, finalPosition);
+    var initialPosition = document.querySelector('header').getBoundingClientRect().bottom || scrollContent.offsetTop;
+    var finalPosition = document.querySelector('footer').getBoundingClientRect().top || scrollContent.offsetHeight;
 
     var stickyNav = function () {
       var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
