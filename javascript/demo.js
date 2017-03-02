1ddf1404ba909fe1a9e7d6fe22fa4c59100e8ecf
Imanuel Gittens <gittensimanuel@gmail.com>
2017-03-01 21:47:37 -0400

    Update demo component to help component.

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,34 +1,45 @@
 (function(){
 
     /* Get all demo items from page */
-    var demoItems = document.querySelectorAll('[c-demo-order^="demo"]');
+    var demoItems = document.querySelectorAll('.c-tooltip-help');
 
     /* Add items from nodeList into an array */
 
-    var i,j;
+    var i;
     var numDemos = demoItems.length;
 
     var demoItemsArray = [];
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
     for(i=0; i < numDemos; i++){
-        demoItemsArray.push(demoItems[i]);
+        demoItems[i].innerHTML = '<span class="c-tooltip-help__message-icon"><i class="fa fa-question-circle-o" aria-hidden="true"></i></span>' + demoItems[i].innerHTML;
+        /*demoItems[i].addEventListener('mouseover', function(){
+            if(this.style.right > width + 'px'){
+                this.style.right = width + 'px';
+            }
+        });*/
+
     }
 
-    /* Sort array to ensure we have the correct ordering */
-
-    demoItemsArray.sort(function(a,b){
-        if (a.attributes['c-demo-order'] < b.attributes['c-demo-order'])
-            return -1;
-        if (a.attributes['c-demo-order'] > b.attributes['c-demo-order'])
-            return 1;
-        return 0;
+    var block4 = document.querySelector('.block4');
+    var block4Tooltip = block4.querySelector('.c-tooltip-help__message-icon');
+    var block4Message = block4.querySelector('.c-tooltip-help__message');
+    var leftOffSet = block4Message.offsetLeft;
+    var screenwidth = block4Message.clientWidth;
+    var scrollX;
+    block4Tooltip.addEventListener('mouseover', function(){
+        scrollX = window.scrollX;
+        if(scrollX > (screenwidth)){
+            console.log('wider than screen');
+        }
+        console.log(leftOffSet);
     });
 
-    for(j = 0; j < demoItemsArray.length; j++){
-        console.log(demoItemsArray[j].children);
-    }
 
-  /*  console.log(demoItemsArray);*/
+
+    console.log(width);
+    console.log(height);
 
 
 })();
