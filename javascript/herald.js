6078555ce7596b48c48c157d089042f36b136b51
Azureowl <maiaroche16@gmail.com>
2017-03-02 16:29:38 -0800

    Completed innovative component

--- javascript/herald.js
+++ javascript/herald.js
@@ -5,18 +5,23 @@
       this.bindEvent();
     },
     cacheDOM: function () {
+      this.chip = document.getElementsByClassName("c-chip__media--o")[0];
       this.classes = {
         chip: document.getElementsByClassName("herald__chip")[0],
         scroll: document.getElementsByClassName("herald__scroll")[0],
+        chipValue: document.getElementsByClassName("c-chip__media--o")[0].innerHTML
       };
     },
     bindEvent: function () {
-      this.classes.chip.addEventListener("click",this.test.bind(this));
+      this.classes.chip.addEventListener("click",this.announce.bind(this));
     },
-    test: function () {
+    announce: function () {
       this.classes.scroll.classList.toggle("herald-announce");
+      this.chip.innerHTML = "X";
+      if (!this.classes.scroll.classList.contains("herald-announce")) {
+        this.chip.innerHTML = this.classes.chipValue;
+      }
     }
   };
   herald.init();
-
 })();
