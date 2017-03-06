d90fa87b34dff704235dffdf393f3e73998e6f90
Jing Ran <jingran.mavis@gmail.com>
2017-03-06 18:31:45 -0500

    Update modal component

--- javascript/modals.js
+++ javascript/modals.js
@@ -1,42 +1,57 @@
 (function() {
   var modalTriggers = document.querySelectorAll('.c-modal-trigger');
+  var modalTriggersArr = [].slice.call(modalTriggers);
   var fullModalTrigger = document.querySelector('.c-modal-trigger--f');
   var closeBtns = document.querySelectorAll('.c-modal__close');
+  var closeBtnsArr = [].slice.call(closeBtns);
   var overlay = document.querySelector('.c-modal-overlay');
-  var triggedId;
   var activeModal;
+  var transitionClass;
+  var overlayClass;
+  var i;
 
-  function trigOverlay(classNm) {
-    activeModal.classList.add('c-modal--active');
-    overlay.classList.add(classNm);
-    overlay.style.visibility = 'visible';
-  }
 
-  function closeOverlay() {
-    activeModal.classList.remove('c-modal--active');
-    overlay.classList.remove('c-modal-overlay--gray');
-    overlay.classList.remove('c-modal-overlay--full');
+  function closeModal() {
+    activeModal.classList.remove(transitionClass);
+    overlay.classList.remove(overlayClass);
     overlay.style.visibility = 'hidden';
   }
 
+  function addOverlayHandler() {
+    overlay.addEventListener('click', closeModal);
+  }
 
-  fullModalTrigger.addEventListener('click', function() {
-    triggedId = this.getAttribute('href').slice(1);
-    activeModal = document.getElementById(triggedId);
-    trigOverlay('c-modal-overlay--full');
-  });
-
-  for (var i = 0; i < modalTriggers.length; i++) {
-    modalTriggers[i].addEventListener('click', function() {
-      triggedId = this.getAttribute('href').slice(1);
-      activeModal = document.getElementById(triggedId);
-      trigOverlay('c-modal-overlay--gray');
+  function addCloseHandler(closeBtn) {
+    closeBtn.addEventListener('click', function() {
+      closeModal();
     });
   }
 
-  for (var j = 0; j < closeBtns.length; j++) {
-    closeBtns[j].addEventListener('click', closeOverlay);
+  function addTriggerHandler(trigger) {
+    var index = modalTriggersArr.indexOf(trigger);
+
+    trigger.addEventListener('click', function() {
+      if (index == 1) {
+        overlayClass = 'c-modal-overlay--full';
+      } else {
+        overlayClass = 'c-modal-overlay--gray';
+      }
+      activeModal = document.getElementById(this.getAttribute('href').slice(1));
+      transitionClass = 'c-modal-transition--' + index;
+      activeModal.classList.add(transitionClass);
+      overlay.classList.add(overlayClass);
+      overlay.style.visibility = 'visible';
+    });
   }
 
-  overlay.addEventListener('click', closeOverlay);
+  for (i = 0; i < modalTriggers.length; i++) {
+    addTriggerHandler(modalTriggers[i]);
+  }
+
+  for (i = 0; i < closeBtns.length; i++) {
+    addCloseHandler(closeBtns[i]);
+  }
+
+  addOverlayHandler();
+
 })();
