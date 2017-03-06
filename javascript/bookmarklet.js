8634373fecec6ccc9f80d6b99b7413ca46b6291d
Sam Chittenden <sbchittenden@gmail.com>
2017-03-06 13:40:24 -0700

    Attempt at adding bookmarks with a button.  Roll to previous commit if things start failing

--- javascript/bookmarklet.js
+++ javascript/bookmarklet.js
@@ -2,20 +2,29 @@
   window.addEventListener('keydown', _keysDown, false);
   window.addEventListener('keyup', _keysUp, false);
 
+  document.addEventListener('selectionchange', _checkForSelection, false);
+
 
   var bookmarkListComponent = document.querySelector('.c-bookmarklet');
   var bookmarkList = bookmarkListComponent.querySelector('.c-bookmaklet__bookmark-list');
   var highlightColor = bookmarkListComponent.dataset.highlightColor;
   var clearAllBtn = bookmarkListComponent.querySelector('.c-bookmarklet__clear-all');
+  var selectionPopUp = document.querySelector('.c-bookmarklet__pop-up');
+  var selectionPopUpPip = selectionPopUp.querySelector('.c-bookmarklet__pop-up-pip');
+  var popUpYesBtn = selectionPopUp.querySelector('.c-bookmarklet__pop-up-button--yes');
+  var popUpNoBtn = selectionPopUp.querySelector('.c-bookmarklet__pop-up-button--no');
   var keysPressed = [];
   var bookmarkId = 0;
+  // var range;
+  // var selection = window.getSelection();
 
   function _keysDown(e) {
     keysPressed[e.keyCode] = true;
 
     // keyboard shortcut Shift(16) + Control(17) + M(77)
     if (keysPressed[16] && keysPressed[17] && keysPressed[77] && keysPressed[76]) {
-      _bookmarkSelection();
+      selectedText = window.getSelection().getRangeAt(0);
+      _bookmarkSelection(selectedText);
     }
 
   }
@@ -24,12 +33,75 @@
     keysPressed[e.keyCode] = false;
   }
 
-  function _bookmarkSelection() {
+  function _checkForSelection() {
+    document.addEventListener('mouseup', function() {
+      var selection = window.getSelection();
+      var selectedRange = selection.getRangeAt(0);
+      _setPopUp(selectedRange);
+    }, false);
+  }
+
+  function _setPopUp(selectedRange) {
+    if (selectedRange.startOffset !== selectedRange.endOffset) {
+      selectionPopUpPip.style.left = "";
+      selectionPopUpPip.style.right = "";
+      selectionPopUp.style.left = "";
+      selectionPopUp.style.right = "";
+
+
+
+      range = selectedRange;
+      var rangeDims = range.getClientRects();
+      var pageWidth = window.innerWidth;
+      var popUpTop = rangeDims[0].top + rangeDims[0].height + 10;
+      var popUpLeft = rangeDims[0].left;
+      var popUpRight = rangeDims[0].width;
+      var pipPosition = rangeDims[0].width / 3;
+      selectionPopUp.style.top = popUpTop + 'px';
+
+      if (pageWidth - 200 < popUpLeft) {
+        // popUpRight = popUpRight - 175;
+        selectionPopUp.style.right = popUpRight + 'px';
+        selectionPopUpPip.style.right = pipPosition + 'px';
+      } else {
+        selectionPopUp.style.left = popUpLeft + 'px';
+        selectionPopUpPip.style.left = pipPosition + 'px';
+      }
+
+      // console.log('selectedRange in _setPopUp', selectedRange);
+      _bookmarkFromButton(selectedRange);
+    }
+  }
+
+  function _bookmarkFromButton(selectedRange) {
+    console.log('bookmark from button selectedRange: ', selectedRange);
+    var rangeStart = selectedRange.startOffset;
+    var rangeEnd = selectedRange.endOffset;
+    var rangeToBookmark = selectedRange.cloneRange();
+    console.log('rangeToBookmark: ', rangeToBookmark, 'selectedRange: ', selectedRange);
+    popUpYesBtn.addEventListener('click', _pressedYes(rangeToBookmark), false);
+    popUpNoBtn.addEventListener('click', function() {
+      console.log('nope');
+    }, false);
+
+
+  }
+
+  function _pressedYes(r) {
+    return function(e) {
+      console.log('pressed Yes: ', r);
+      _bookmarkSelection(r);
+    }
+  }
+
+
+  function _bookmarkSelection(selectedRange) {
     // console.log('Shortcut initiated!');
-    var selection;
+    // var selection;
+    console.log('selection inside _bookmarkSelection function: ', selectedRange);
     var alertNode;
     var selectionString;
-    var selectedText;
+    // var selectedText;
     var highlightNode;
     var highlightNodeQueryString;
     var bookmarkAnchorLink;
@@ -39,7 +111,7 @@
 
     alertNode = bookmarkListComponent.parentNode;
 
-    selection = window.getSelection();
+    // selection = selectionObj || window.getSelection();
 
     // create span node for text hightlighting
     highlightNode = document.createElement('span');
@@ -50,15 +122,16 @@
       highlightNode.style = 'background-color: ' + highlightColor;
     }
 
-    if (selection.anchorNode) {
+    // if (selection.anchorNode) {
+    if (selectedRange) {
 
       // get the range of selected text and surround it with the hightlighting span
-      selectedText = selection.getRangeAt(0);
+      // selectedText = selection.getRangeAt(0);
 
-      if (selectedText.startContainer === selectedText.endContainer && selectedText.startOffset !== selectedText.endOffset) {
+      if (selectedRange.startContainer === selectedRange.endContainer && selectedRange.startOffset !== selectedRange.endOffset) {
 
         // surround selected text with hightlighting span
-        selectedText.surroundContents(highlightNode);
+        selectedRange.surroundContents(highlightNode);
 
         // add unique id to highlighted span
         highlightNode.setAttribute('id', 'bookmarklet_' + bookmarkId);
@@ -109,7 +182,7 @@
         bookmarkListComponent.classList.add('c-bookmarklet--is-visible');
 
       } else {
-        if (selectedText.startContainer !== selectedText.endContainer) {
+        if (selectedRange.startContainer !== selectedRange.endContainer) {
 
           console.log("Don't cross the streams!! (nodes have been split)");
           // create background div and add class
@@ -126,7 +199,7 @@
           alertNode.appendChild(warning);
           alertNode.appendChild(background);
         }
-        if (selectedText.startOffset === selectedText.endOffset) {
+        if (selectedRange.startOffset === selectedRange.endOffset) {
           console.log("Nothing selected!");
           // create background div and add class
           var background = document.createElement('div');
@@ -146,7 +219,7 @@
       }
 
       // convert selection to string
-      selectionString = selection.toString();
+      // selectionString = selection.toString();
 
     }
     // create remove list item listeners
