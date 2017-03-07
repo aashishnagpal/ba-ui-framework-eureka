43866cb9e68df5eb4184e8825becd7e32f764253
Azureowl <maiaroche16@gmail.com>
2017-03-07 14:15:53 -0800

    Remove line break, condensed wtf js files, and added demo only js

--- javascript/DEMOONLYwtf.js
+++ javascript/DEMOONLYwtf.js
@@ -1,4 +1,24 @@
 (function () {
+  var demoWTF = {
+    init: function () {
+      this.cacheDOM();
+      this.bindEvents();
+    },
+    cacheDOM: function () {
+      this.wtfBtn = document.getElementById("wtf-try");
+      this.component = document.getElementsByClassName("c-wtf")[0];
+      this.component.style.display = "none";
+    },
+    bindEvents: function () {
+      this.wtfBtn.addEventListener("click", this.activateWTF.bind(this));
+    },
+    activateWTF: function () {
+      this.component.style.display = "block";
+      dictionaryLinker.init();
+    }
+  };
+  demoWTF.init();
+  
   var dictionaryLinker = {
     init: function () {
       this.cacheDOM();
@@ -44,5 +64,4 @@
       this.ol.innerHTML = "";
     }
   };
-    dictionaryLinker.init();
 })();
