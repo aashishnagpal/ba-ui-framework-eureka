76d45d6b7df05e5ae50d153b83bbf6925276096a
Sam Chittenden <sbchittenden@gmail.com>
2017-03-08 11:13:53 -0700

    Fix for side nav

---
+++ javascript/demo-sticky-nav.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Aashish on 2/25/2017.
+ */
+(function() {
+  'use strict';
+
+
+  var fetchStickyNavBoundaries = function() {
+
+    var getPosition = function(element, dimension) {
+      return window.pageYOffset + element.getBoundingClientRect()[dimension];
+    };
+
+    // fetch the navigation element (can be any element, list or not)
+    // must have data-nav-type="sticky"
+    var nav = document.querySelector('[data-nav-type="sticky"]');
+    if (nav !== null) {
+      // Fetch header element using header id
+      // get bottom of header to set starting boundary or fallback to Nav element's top
+
+      // fetch the element on which the this navigation spies on.
+      // The element may not be present, then fallback to body element
+      var scrollContent = document.getElementById(nav.getAttribute('data-content-id')) || document.documentElement || document.body;
+
+      var siteHeader = document.getElementById(nav.getAttribute('data-header-id')); // Header Element
+      var initialPosition = siteHeader ? getPosition(siteHeader, 'bottom') : getPosition(scrollContent, 'top');
+
+      // Fetch footer element using footer id
+      // get top of footer to set ending boundary or fallback to scrollContent's bottom
+      var siteFooter = document.getElementById(nav.getAttribute('data-footer-id'));
+      var finalPosition = siteFooter ? getPosition(siteFooter, 'top') : getPosition(scrollContent, 'bottom');
+
+      return {
+        nav: nav,
+        start: initialPosition,
+        end: finalPosition - window.innerHeight
+      };
+    }
+  };
+
+  var stickyNav = function(boundaries) {
+    console.log('nav: ', boundaries.nav);
+    // read current scroll position
+    var scrollPosition = window.pageYOffset;
+    console.log(scrollPosition, boundaries.end);
+    // test scroll position against starting and ending boundaries and add or remove sticky nature
+    if (scrollPosition < boundaries.start) {
+      if (boundaries.nav.classList.contains('nav--sticky')) {
+        boundaries.nav.classList.remove('nav--sticky');
+      }
+      if (boundaries.nav.classList.contains('nav-sticky--bottom')) {
+        boundaries.nav.classList.remove('nav-sticky--bottom');
+      }
+
+    } else if (scrollPosition > boundaries.end) {
+      boundaries.nav.classList.remove('nav--sticky');
+      boundaries.nav.classList.add('nav-sticky--bottom');
+      boundaries.nav.style.bottom = scrollPosition - boundaries.end + 'px';
+    } else {
+      boundaries.nav.classList.add('nav--sticky');
+    }
+  };
+
+  window.addEventListener('load', function() {
+    var stickyBoundaries = fetchStickyNavBoundaries();
+    window.addEventListener('scroll', function() {
+      stickyNav(stickyBoundaries);
+    });
+  });
+})();
