c9c8c0311ca5557decc9e037229b5980f5241ff6
Jing Ran <jingran.mavis@gmail.com>
2017-03-12 13:39:14 -0400

    Update inline note

--- javascript/inline-note.js
+++ javascript/inline-note.js
@@ -8,9 +8,16 @@
 
 
   function createNote() {
-    var selectedTxt = window.getSelection();
+    var selectedRange = window.getSelection().getRangeAt(0);
 
-    if (selectedTxt !== '') {
+    if (selectedRange.startContainer === selectedRange.endContainer
+        && selectedRange.startOffset !== selectedRange.endOffset) {
+      notes[index] = {
+        'selectedText': selectedRange.toString(),
+        'note': ''
+      };
+
+      console.log(notes[index]);
       var highlight = document.createElement('span');
       var note = document.createElement('textarea');
       highlight.className = 'c-inline-note__highlight';
@@ -21,35 +28,52 @@
       activeIndex = index;
       index++;
       // highlight the selected text
-      selectedTxt.getRangeAt(0).surroundContents(highlight);
+      selectedRange.surroundContents(highlight);
+
       // add note
       highlight.appendChild(note);
 
       // add open feature to highlight
       highlight.addEventListener('click', function(e) {
         console.log("style: " + note.style.display);
+        var highlightId = this.getAttribute('id');
+        if (highlightId.slice(2) === "" + activeIndex) return;
         e.stopPropagation();
-
-        // note.style.display = 'block';
-        openNote(this.getAttribute('id'));
+        openNote(highlightId);
       });
+
+      note.addEventListener('click', function(e) {
+        e.stopPropagation();
+      });
+    } else {
+      console.log('invalid');
     }
   }
   
   function openNote(highlightId) {
+    console.log('hightid ' + highlightId);
+    closeNote();
     var i = highlightId.slice(2);
     document.getElementById('note' + i).style.display = 'block';
     activeIndex = i;
   }
   
   function closeNote() {
+    console.log('active index in close ' + activeIndex);
     if (activeIndex === undefined) return;
-    document.getElementById('note' + activeIndex).style.display = 'none';
-
-    // activeIndex = undefined;
+    var activeNote = document.getElementById('note' + activeIndex);
+    activeNote.style.display = 'none';
+    // update note key value in notes obj
+    notes[activeIndex].note = activeNote.value;
+    console.log(notes[activeIndex]);
+    console.log(JSON.stringify(notes));
+    activeIndex = undefined;
   }
   function removeNote() {
-    
+    console.log('remove ' + activeIndex);
+    if (activeIndex === undefined) return;
+
+    var hightlighted = document.getElementById('hl' + activeIndex);
   }
   function clearNotes() {
     
@@ -60,11 +84,17 @@
 
   document.body.addEventListener('click', closeNote);
 
-  // If shift, control and i keys are all pressed down, create a note
+
   document.addEventListener('keydown', function(e) {
+    // If shift, control and i keys are all pressed down, create a note
     if (e.keyCode === 73 && e.shiftKey && e.ctrlKey) {
       closeNote();
       createNote();
+    }
+
+    // If shift, control and d keys are all pressed down, remove a note
+    if (e.keyCode === 68 && e.shiftKey && e.ctrlKey) {
+      removeNote();
     }
   });
 })();
